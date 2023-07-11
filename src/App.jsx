261e7c30045537771b9badc1b6437137778cd58b
Franco Piscoya <114585682+fr4ncopiscoya@users.noreply.github.com>
2023-07-11 10:11:04 -0500

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import freeCodeCampLogo from './img/freecodecamp-logo.png';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="logo-container">
+        <img src={freeCodeCampLogo} alt="" className="logo" />
+        <div className="main-container">
+          
+        </div>
+      </div> 
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Button.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import '../css/Testimonio.css'
+
+function Button({texto, btnaction}) {
+    return (
+        <>
+        <button className="">
+            {texto}
+        </button>
+        </>
+    );
+}
+
+export default Testimonio;
